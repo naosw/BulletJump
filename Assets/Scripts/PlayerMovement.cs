6d17db429a31f903323b5b4692a253a9e0b1e05d
naosw <naoswell@hotmail.com>
2024-05-14 15:50:45 -0400

    added super simple jump limit with gun barrel

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,7 +8,13 @@ public class PlayerMovement : MonoBehaviour
     public float jumpForce = 6f;
     public float walkSpeed = 6f;
     public TextMeshProUGUI bullets;
-
+    public GameObject chamberOne;
+    public GameObject chamberTwo;
+    public GameObject chamberThree;
+    public GameObject chamberFour;
+    public GameObject chamberFive;
+    public GameObject chamberSix;
+    public GameObject chamberEmpty;
     private int bulletCount = 6;
 
     Rigidbody2D rb;
@@ -37,6 +43,42 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.D))
         {
             rb.velocity = new Vector2(walkSpeed, rb.velocity.y);
+        }
+
+        // bullet chambers
+        if (bulletCount == 6) {
+            chamberSix.SetActive(true);
+            chamberEmpty.SetActive(false);
+        }
+        if (bulletCount == 5)
+        {
+            chamberFive.SetActive(true);
+            chamberSix.SetActive(false);
+        }
+        if (bulletCount == 4)
+        {
+            chamberFour.SetActive(true);
+            chamberFive.SetActive(false);
+        }
+        if (bulletCount == 3)
+        {
+            chamberThree.SetActive(true);
+            chamberFour.SetActive(false);
+        }
+        if (bulletCount == 2)
+        {
+            chamberTwo.SetActive(true);
+            chamberThree.SetActive(false);
+        }
+        if (bulletCount == 1)
+        {
+            chamberOne.SetActive(true);
+            chamberTwo.SetActive(false);
+        }
+        if (bulletCount == 0)
+        {
+            chamberEmpty.SetActive(true);
+            chamberOne.SetActive(false);
         }
     }
 }
